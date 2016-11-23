68e953670080ec3ec7ae6e1f9bbf5a08f031dd4a
Daniel Vedin <dv222bk@student.lnu.se>
2016-11-23 21:05:06 +0100

    RED

---
+++ YahtzeeTDDTest/DiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using YahtzeeTDD;
+
+namespace YahtzeeTDDTest
+{
+    [TestClass]
+    public class DiceTests
+    {
+        private Dice sut = new Dice();
+
+        [TestMethod]
+        public void RollShouldGenerateValidRandomNumbers()
+        {
+            var mock = new Mock<Random>();
+
+            sut.Roll();
+            mock.Verify(m => m.Next(1, 6), Times.Once());
+        }
+    }
+}
