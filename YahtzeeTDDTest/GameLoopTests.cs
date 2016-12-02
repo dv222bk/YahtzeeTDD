3fca967c7c2fa036945c057ca6c5e4bd3d80f3c4
Daniel Vedin <dv222bk@student.lnu.se>
2016-12-02 15:47:44 +0100

    RED

--- YahtzeeTDDTest/GameLoopTests.cs
+++ YahtzeeTDDTest/GameLoopTests.cs
@@ -79,5 +79,29 @@ namespace YahtzeeTDDTest
             MockView.Verify(m => m.ReadInput(), Times.Once);
             MockLogic.Verify(m => m.ReactToStandardInput("input"), Times.Once);
         }
+
+        [TestMethod]
+        public void LoopShouldCallTheProperReactToMethodAfterReactToStandardInput()
+        {
+            int orderOfCalls = 0;
+            MockLogic.Setup(m => m.ReactToStandardInput(It.IsAny<String>())).Callback(() => orderOfCalls++);
+            MockLogic.Setup(m => m.ReactToPlayingInput(It.IsAny<String>())).Callback(() => Assert.AreEqual(orderOfCalls++, 1));
+            MockLogic.Setup(m => m.ReactToSavingInput(It.IsAny<String>())).Callback(() => Assert.AreEqual(orderOfCalls++, 3));
+            MockLogic.Setup(m => m.ReactToSaveDieInput(It.IsAny<String>())).Callback(() => Assert.AreEqual(orderOfCalls++, 5));
+
+            MockLogic.Object.State = State.Playing;
+            sut.Loop();
+            MockLogic.Object.State = State.Saving;
+            sut.Loop();
+            MockLogic.Object.State = State.SaveDie;
+            sut.Loop();
+            MockLogic.Object.State = State.Start;
+            sut.Loop();
+
+            MockLogic.Verify(m => m.ReactToStandardInput(It.IsAny<String>()), Times.Exactly(4));
+            MockLogic.Verify(m => m.ReactToPlayingInput(It.IsAny<String>()), Times.Once);
+            MockLogic.Verify(m => m.ReactToSavingInput(It.IsAny<String>()), Times.Once);
+            MockLogic.Verify(m => m.ReactToSaveDieInput(It.IsAny<String>()), Times.Once);
+        }
     }
 }
