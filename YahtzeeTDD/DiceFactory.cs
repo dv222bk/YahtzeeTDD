28959bb9e185998e1cd948113cc635222b4e4a6f
Daniel Vedin <dv222bk@student.lnu.se>
2016-12-05 12:43:33 +0100

    RED
    So I noticed I hadn't actually made something that create an object
    other than at setup, so I'll just create a quick new DiceSet class that
    does this to show that I can :)

---
+++ YahtzeeTDD/DiceFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YahtzeeTDD
+{
+    public class DiceFactory
+    {
+        Random Random;
+
+        public DiceFactory(Random random)
+        {
+            Random = random;
+        }
+
+        public object CreateDice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ YahtzeeTDDTest/DiceFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YahtzeeTDD;
+
+namespace YahtzeeTDDTest
+{
+    [TestClass]
+    public class DiceFactoryTests
+    {
+        Random random = new Random();
+        DiceFactory sut = new DiceFactory(new Random());
+
+        public DiceFactoryTests()
+        {
+            sut = new DiceFactory(random);
+        }
+
+        [TestMethod]
+        public void CreateDiceShouldReturnADiceObject()
+        {
+            var expected = new Dice(random);
+            var actual = sut.CreateDice();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
