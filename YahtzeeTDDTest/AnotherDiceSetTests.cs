411ee48d4a2c8158fb3cc7f9d98990a310f0c056
Daniel Vedin <dv222bk@student.lnu.se>
2016-12-05 13:16:05 +0100

    REFACTOR

--- YahtzeeTDDTest/AnotherDiceSetTests.cs
+++ YahtzeeTDDTest/AnotherDiceSetTests.cs
@@ -8,38 +8,39 @@ namespace YahtzeeTDDTest
     [TestClass]
     public class AnotherDiceSetTests
     {
+        Mock<Dice>[] MockDiceSet = new Mock<Dice>[5];
+        Dice[] MockDiceObjectSet = new Dice[5];
         Mock<DiceFactory> mockFactory = new Mock<DiceFactory>(new Random());
         AnotherDiceSet sut;
 
         public AnotherDiceSetTests()
         {
+            for (int i = 0; i < 5; i += 1)
+            {
+                MockDiceSet[i] = new Mock<Dice>(new Random());
+                MockDiceObjectSet[i] = MockDiceSet[i].Object;
+            }
             sut = new AnotherDiceSet(mockFactory.Object);
         }
 
         [TestMethod]
         public void RollAllShouldFillDiceSetWithNewDice()
         {
-            Random random = new Random();
-            Dice[] expected = new Dice[5];
-            for (int i = 0; i < expected.Length; i += 1)
-            {
-                expected[i] = new Dice(random);
-            }
             mockFactory.SetupSequence(m => m.CreateDice())
-                .Returns(expected[0])
-                .Returns(expected[1])
-                .Returns(expected[2])
-                .Returns(expected[3])
-                .Returns(expected[4]);
+                .Returns(MockDiceObjectSet[0])
+                .Returns(MockDiceObjectSet[1])
+                .Returns(MockDiceObjectSet[2])
+                .Returns(MockDiceObjectSet[3])
+                .Returns(MockDiceObjectSet[4]);
 
             sut.RollAll();
 
             mockFactory.Verify(m => m.CreateDice(), Times.Exactly(5));
-            CollectionAssert.AreEqual(expected, sut.DiceSet);
+            CollectionAssert.AreEqual(MockDiceObjectSet, sut.DiceSet);
         }
         
         [TestMethod]
-        public void RollAllShouldNotFillDiceSetIfCurrentRollEqualsMaxRoll()
+        public void RollAllShouldNotDoAnythingIfCurrentRollEqualsMaxRoll()
         {
             sut.CurrentRoll = 3;
             sut.RollAll();
